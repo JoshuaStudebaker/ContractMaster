f4ee950cdc59183e354813b96b369516a2e31220
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-10-19 13:50:57 -0600

    start

---
+++ Models/Job.cs
@@ -0,0 +1,11 @@
+namespace ContractMaster.Models
+{
+  public class Job
+  {
+    public int Id { get; set; }
+    public string Location { get; set; }
+    public string Description { get; set; }
+    public string Contact { get; set; }
+
+  }
+}
