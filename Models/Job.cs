fb6a97eb1f93b42be0bb89385b557d7b2e79a3c4
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-10-19 13:56:27 -0600

    added job model

--- Models/Job.cs
+++ Models/Job.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ContractMaster.Models
 {
   public class Job
   {
     public int Id { get; set; }
-    public string Location { get; set; }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    [MinLength(2)]
+    [MaxLength(2)]
+    public string State { get; set; }
+    [MinLength(5)]
+    [MaxLength(5)]
+    public int ZipCode { get; set; }
+    [Required]
     public string Description { get; set; }
-    public string Contact { get; set; }
+    public string ContactName { get; set; }
+    public string ContactPhone { get; set; }
 
   }
 }
